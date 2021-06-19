006c0c7561b31f794273c494c79a09226791f01a
wpMcDonnell <will.mcdonnell4@gmail.com>
2021-06-18 22:07:05 -0400

    Add abi from compile of Token contract

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -3,9 +3,9 @@ const dBank = artifacts.require("dBank");
 
 module.exports = async function(deployer) {
 	//deploy Token
-
+	await deployer.deploy(Token)
 	//assign token into variable to get it's address
-	
+
 	//pass token address for dBank contract(for future minting)
 
 	//assign dBank contract into variable to get it's address
--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -5,17 +5,28 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
   //add minter variable
+  address public minter;
 
   //add minter changed event
+  event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
+  constructor() public payable ERC20("Decentralized Bank Currency", "DCB") {
+    minter = msg.sender;
+  }
+
+  function passMinterRole(address dBank) public returns (bool) {
+    require(msg.sender==minter, 'Error, only owner can change pass minter role');
+    minter = dBank;
+
+    emit MinterChanged(msg.sender, dBank);
+    return true;
   }
 
   //Add pass minter role function
 
   function mint(address account, uint256 amount) public {
     //check if msg.sender have minter role
+    require(msg.sender == minter, 'Error, msg.sender does not have minter role');
 		_mint(account, amount);
 	}
 }
