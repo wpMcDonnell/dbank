dbfe449ffbb15671124d2b9035f3e75efd1a4ad2
wpMcDonnell <will.mcdonnell4@gmail.com>
2022-05-27 15:30:40 -0400

    Deployed to rinkeby, added README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# DeFi Bank
+
+Used React App to connect via web3 to deployed rinkeby contracts
+
+## Deployed information
+
+<br /> 
+
+![Deployed Info](./public/deployed_info.png)
--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,10 @@
-require('babel-register');
-require('babel-polyfill');
-require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider-privkey');
-const privateKeys = process.env.PRIVATE_KEYS || ""
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const fs = require('fs');
+// Infura Key used for Ethereum
+const infuraKey = fs.readFileSync(".secret").toString().trim();
+// Alchemy Key used for mumbai
+const alchemyKey = fs.readFileSync(".secretTwo").toString().trim();
+const mnemonic = fs.readFileSync(".env").toString().trim();
 
 module.exports = {
   networks: {
@@ -11,50 +13,35 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
-    kovan: {
-      provider: function() {
-        return new HDWalletProvider(
-          privateKeys.split(','), // Array of account private keys
-          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
-        )
-      },
-      gas: 5000000,
-      gasPrice: 5000000000, // 5 gwei
-      network_id: 42
-    },
-    main: {
-      provider: function() {
-        return new HDWalletProvider(
-          privateKeys.split(','), // Array of account private keys
-          `https://main.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
-        )
-      },
-      gas: 5000000,
-      gasPrice: 5000000000, // 5 gwei
-      network_id: 1
-    },
     rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(
-          privateKeys.split(','), // Array of account private keys
-          `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
-        )
-      },
-      gas: 5000000,
-      gasPrice: 5000000000, // 5 gwei
-      network_id: 4
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        // Rinkeby id
+        network_id: 4,
+        gas: 8500000,
+        from: "0x83ca0B46a5D5CeD7420285d1252d3348649bF5fC",
+        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+        skipDryRun: true
     },
     ropsten: {
-      provider: function() {
-        return new HDWalletProvider(
-          privateKeys.split(','), // Array of account private keys
-          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`// Url to an Ethereum Node
-        )
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
+        // Ropsten's id
+        network_id: 3,
+        from: "0x83ca0B46a5D5CeD7420285d1252d3348649bF5fC",       
+        gas: 466000,        // Ropsten has a lower block limit than mainnet
+        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
       },
-      gas: 5000000,
-      gasPrice: 5000000000, // 5 gwei
-      network_id: 3
-    }
+    // Polygon Testnet
+    mumbai: {
+      provider: () => new HDWalletProvider(mnemonic, `https://polygon-mumbai.g.alchemy.com/v2/${alchemyKey}`),
+        // Mumbai id
+        network_id: 80001,
+        confirmations: 2,
+        timeoutBlocks: 200,
+        skipDryRun: true   // Skip dry run before migrations? (default: false for public nets )
+    },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
