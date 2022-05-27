a37bf6d5bee5deed71fa268f779fee2d48e40491
wpMcDonnell <will.mcdonnell4@gmail.com>
2022-05-27 14:43:00 -0400

    Replace icon, change title, change href for logo

--- src/components/App.js
+++ src/components/App.js
@@ -96,7 +96,7 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="https://wpmcdonnell.github.io/"
             target="_blank"
             rel="noopener noreferrer"
           >
