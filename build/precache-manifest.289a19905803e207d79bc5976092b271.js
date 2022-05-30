c3de3485a67a2e508cee7e07b03d79c2afe4ebab
wpMcDonnell <will.mcdonnell4@gmail.com>
2022-05-30 17:28:17 -0400

    Push build to main repo

---
+++ build/precache-manifest.289a19905803e207d79bc5976092b271.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16eff293329d289690911257ab94d028",
+    "url": "/dbank/index.html"
+  },
+  {
+    "revision": "63c87edea4c077297cbb",
+    "url": "/dbank/static/css/2.963765e0.chunk.css"
+  },
+  {
+    "revision": "2e982434f34b865fd542",
+    "url": "/dbank/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "63c87edea4c077297cbb",
+    "url": "/dbank/static/js/2.d1e50f86.chunk.js"
+  },
+  {
+    "revision": "9143a7c0cbfa518ce39da65db7eaf56a",
+    "url": "/dbank/static/js/2.d1e50f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e982434f34b865fd542",
+    "url": "/dbank/static/js/main.acd95de7.chunk.js"
+  },
+  {
+    "revision": "752f5eee0f61edd30a79",
+    "url": "/dbank/static/js/runtime-main.f33ada91.js"
+  },
+  {
+    "revision": "adca41d3777e83416e7ad919fa0da8ac",
+    "url": "/dbank/static/media/dbank.adca41d3.png"
+  }
+]);
